8b8c444c2c173c611c626073a4c336429496563f
Onyangonashon <nashononyango274@gmail.com>
2026-04-15 18:39:47 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+//example1//
+odd = [13,27]
+print(f"The numbers stored in 'odd' are: {odd}")
+//example2//
+num1 = 5
+num2 = 10
+var console(num1 {num1});
+var console(num2 {num2});
+result = num1 * num2
+
+print(f"The result of multiplying {num1} and {num2} is: {result}")
+//example3//
+let age=25
+console.log(25){aage};
+print(f"The value of age is: {age}"
+
+    Food= pizza
+    print(f"my fevorite food[food])
+//example4//
+let length = 10
+let width = 5
+ P = 2 * (l + w)
+perimeter = 2 * (length + width)
+
+
+print(f"The perimeter of a rectangle with length {length} and width {width} is {perimeter}")
+//example5//
+items=maize&beans
+c0nsole.log(item{item});
+price=maize 500
+price =beans=60
+
+print=(f"The price of maize & beans{item}and{price}")
+//example6//
+
